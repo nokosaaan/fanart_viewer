2069f40a2b856f59bd62556017ec2761f7cd371a
nokosaaan <nisihimura.r.019@ms.saitama-u.ac.jp>
2026-08-07 17:55:55 +0900

    fix: specify raspi mode
    Signed-off-by: nokosaaan <nisihimura.r.019@ms.saitama-u.ac.jp>

--- browser-extension/background.js
+++ browser-extension/background.js
@@ -1,4 +1,4 @@
-const DEFAULT_BACKEND_ORIGIN = 'http://localhost:8000'
+const DEFAULT_BACKEND_ORIGIN = 'https://psi-friendly-moves-literally.trycloudflare.com/'
 const REQUEST_TIMEOUT_MS = 20000
 
 function normalizeOrigin(value) {
