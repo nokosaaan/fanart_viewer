44c9759f9bf28e59babbd7ba2b46aa913053b053
nokosaaan <nishimura.r.019@ms.saitama-u.ac.jp>
2026-08-07 23:57:44 +0900

    fix: url
    Signed-off-by: nokosaaan <nishimura.r.019@ms.saitama-u.ac.jp>

--- browser-extension/background.js
+++ browser-extension/background.js
@@ -1,5 +1,5 @@
 // const DEFAULT_BACKEND_ORIGIN = 'http://localhost:8000'
-const DEFAULT_BACKEND_ORIGIN = 'https://psi-friendly-moves-literally.trycloudflare.com'
+const DEFAULT_BACKEND_ORIGIN = 'https://carrying-cents-purchase-day.trycloudflare.com/'
 const REQUEST_TIMEOUT_MS = 20000
 
 function normalizeOrigin(value) {
